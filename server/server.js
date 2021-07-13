6a907ee3719a997209138c890d589dce5ebb3e87
[Andrew Core] <[adcore89@gmail.com]>
2021-07-12 20:54:23 -0400

    trobule shoot nodemon

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+server.applyMiddleware({ app });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
